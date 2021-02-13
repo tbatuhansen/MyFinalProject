dc89c0b5bad1c2467cc37675ba3bfb2f608880a2
tbatuhansen <47259557+tbatuhansen@users.noreply.github.com>
2021-02-14 01:54:34 +0300

    Second

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq.Expressions;
+using Entities.Abstract;
+
+namespace DataAccess.Abstract
+{
+    //class : referans tip
+    //IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir.
+    //new() : newlenebilir olmalı
+    public interface IEntityRepository<T> where T : class, IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
